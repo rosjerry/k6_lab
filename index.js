fe59964d0ddbd0792bfba49fddc6da292c861c14
EC2 Default User <ec2-user@ip-172-31-23-231.eu-central-1.compute.internal>
2025-06-10 12:11:36 +0000

    go results

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ export default function () {
       'Content-Type': 'application/json',
     },
   };
-  http.post('http://localhost:8080/todos', payload, params);
+  http.post('http://3.71.185.56:8080/todos', payload, params);
 }
 
 export function teardown() {
-  http.get('http://localhost:8080/todos');
+  http.get('http://3.71.185.56:8080/todos');
 }
 
 export const options = {
-  vus: 2,
+  vus: 20,
   duration: '10s',
 };
